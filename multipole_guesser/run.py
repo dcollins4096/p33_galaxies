a0d00411295f493a17018afaea70248bf955aa8a
David Collins <dccollins@fsu.edu>
2025-08-15 14:27:56 -0400

    new run

--- multipole_guesser/run.py
+++ multipole_guesser/run.py
@@ -7,21 +7,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import loader
+import plot_multipole
+reload(plot_multipole)
 reload(loader)
 
 new_model = 1
 load_model = 0
 train_model = 1
-save_model = 1
+save_model = 0
 plot_models = 1
 
 if new_model:
-    import networks.net0013 as net
+    import networks.net0015 as net
     reload(net)
     model = net.thisnet()
 
-sky, clm = loader.loader('clm_take6.h5',ntrain=512, nvalid=3)
+#sky, clm = loader.loader('clm_take6.h5',ntrain=512, nvalid=3)
 #sky, clm = loader.loader('clm_take6.h5',ntrain=4, nvalid=3)
+sky, clm = loader.loader('clm_take7_partial.h5',ntrain=20, nvalid=3)
 
 if load_model:
     model.load_state_dict(torch.load("models/test%d.pth"%net.idd))
@@ -46,18 +49,24 @@ if train_model:
 
 model.idd = net.idd
 if plot_models:
+    ds_train = net.SphericalDataset(sky['train'], clm['train'], fit_stats=True, nside=model.nside)
+    ds_val   = net.SphericalDataset(sky['valid'], clm['valid'], rm_mean=ds_train.rm_mean, rm_std=ds_train.rm_std, fit_stats=False, nside=model.nside) 
     print('ploot')
     err=[]
     delta = []
     fig,ax=plt.subplots(1,3, figsize=(12,4))
-    for sss,ccc in zip( sky['train'],clm['train']):
-        moo = model(sss.unsqueeze(0), pool=True)
-        err.append( model.criterion(moo,ccc.unsqueeze(0)))
-        delta = torch.abs( 1-moo/ccc).detach().numpy()[0]
-        ax[1].plot(delta, marker='*')
-        c1, m1 =  ccc.detach().numpy(),moo[0].detach().numpy()
-        args = np.argsort(c1)
-        ax[2].plot(c1[args],m1[args])
+    for n in range(len(sky['train'])):
+        if 1:
+            rm, clm = ds_train[n]
+            moo = model(rm.unsqueeze(0))
+            err.append( model.criterion(moo,clm.unsqueeze(0)))
+            delta = torch.abs( 1-moo/clm).detach().numpy()[0]
+            ax[1].plot(delta, marker='*')
+            c1, m1 =  clm.detach().numpy(),moo[0].detach().numpy()
+            args = np.argsort(c1)
+            ax[2].plot(c1[args],m1[args])
+
+        #plot_multipole.rmplot( sky['train'][n], rm, fname = "rm_and_sampled_%04d"%n)
 
     err = torch.tensor(err).detach().numpy()
     ax[0].hist(err)
